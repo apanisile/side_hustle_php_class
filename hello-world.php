5cbcd2f0a42ce5fdd8e2e23fbc412916f2a6fffa
apanisile <apanisileifeoluwa@gmail.com>
2021-02-20 08:27:46 +0100

    removed the random number generator

--- hello-world.php
+++ hello-world.php
@@ -1,8 +1,6 @@
 <?php
 
-//echo "<h2> Hello World! </h2><br>";
-//echo "My name is Ifeoluwa, I'll rather prefer to be called Lala <br>";
-//echo "This is my first task for side hustle internship ";
-
-echo(mt_rand(1,100000000000))
+echo "<h2> Hello World! </h2><br>";
+echo "My name is Ifeoluwa, I'll rather prefer to be called Lala <br>";
+echo "This is my first task for side hustle internship ";
 ?>
