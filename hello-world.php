f03d94fe516f80e9331bf1054cc917a70a87c306
apanisile <apanisileifeoluwa@gmail.com>
2021-02-20 08:23:21 +0100

    testing random numbers

--- hello-world.php
+++ hello-world.php
@@ -1,7 +1,8 @@
 <?php
 
-echo "Hello World"
-echo "My name is Ifeoluwa"
-echo "This is a test file for side hustle internship"
+//echo "<h2> Hello World! </h2><br>";
+//echo "My name is Ifeoluwa, I'll rather prefer to be called Lala <br>";
+//echo "This is my first task for side hustle internship ";
 
+echo(mt_rand(1,100000000000))
 ?>
